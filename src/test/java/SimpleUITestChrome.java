56cfe7ae277130ca248d2d8f2f41afe4a94d336d
Boyar Roman <br777roman@gmail.com>
2016-10-27 12:17:24 +0300

    third commit: 1.CreateIssue 2. Login (run beforeTest)

--- src/test/java/SimpleUITestChrome.java
+++ src/test/java/SimpleUITestChrome.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -8,7 +9,7 @@ import org.testng.annotations.Test;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -20,12 +21,12 @@ public class SimpleUITestChrome {
     private final String myPassword = "br777roman";
 
     //the Date for writting simple description
-    private Date date =  new Date();
+    private Date date = new Date();
     private DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
     protected WebDriver driver;
 
-    public SimpleUITestChrome( ) {
+    public SimpleUITestChrome() {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
         driver = new ChromeDriver();
@@ -33,12 +34,12 @@ public class SimpleUITestChrome {
     }
 
     @Test
-    public void createIssueInJiraTest(){
+    public void createIssueInJiraTest() {
 
 //        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 //        driver = new ChromeDriver();
 
-        String  eTitle = "Log in - JIRA",
+        String eTitle = "Log in - JIRA",
                 aTitle = "";
 
         //start browser and go to adres
@@ -57,30 +58,59 @@ public class SimpleUITestChrome {
             e.printStackTrace();
         }
 
+        //выбираем правильную группу
+//        Select select =
+//                new Select(driver.findElement(By.xpath("//*[@id='project-field']")));
+//        select.selectByVisibleText("QAAutomation2 (QAAUT)");
+//        select.selectByValue("QAAutomation2 (QAAUT)");
+
+//        new Select(driver.findElement(By.id("project-field"))).selectByVisibleText("QAAutomation2 (QAAUT)");
+
+        driver.manage().timeouts().implicitlyWait(1000L, TimeUnit.MILLISECONDS);
+
+        WebElement webElement2 = driver.findElement(By.xpath("//*[@id='project-field']"));
+        webElement2.click();
+        webElement2.sendKeys("QAAutomation2 (QAAUT)");
+        webElement2.submit();
+//        driver.findElement(By.id("//*[@id='qaautomation2-(qaaut)-1538']")).click();
+
 
         String issueName = "ISSUE at: " + dateFormat.format(date);
         //Search element
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         driver.findElement(By.xpath("//*[@id='summary']")).sendKeys(issueName);
+        driver.findElement(By.xpath("//*[@id='reporter-field']")).sendKeys(myLogin);
+
         driver.findElement(By.xpath("//*[@id='description']")).sendKeys(issueName);
         driver.findElement(By.xpath("//*[@id='create-issue-submit']")).click();
 
-        if ( driver.findElement(By.xpath("//*[@id='aui-flag-container']/div/div")).isEnabled())
-            System.out.println("You successfully have created new ISSUE by NAME: " +issueName);
+        //все команды поиска автоматически становятся ожидаемыми
+        driver.manage().timeouts().implicitlyWait(1000L, TimeUnit.MILLISECONDS);
 
-   }
-
-
+        if (driver.findElement(By.xpath("//*[@id='aui-flag-container']/div/div")).isEnabled()) {
 
 
+            WebElement webElement = driver.findElement(By.xpath("//*[@id='aui-flag-container']/div/div"));
+            System.out.println(" webElement.getText() = " + webElement.getText());
+            System.out.println("webElement.getTagName() = " + webElement.getTagName());
+        }
+
+        System.out.println("You successfully have created new ISSUE by NAME: " + issueName);
+
+    }
 
 
     @Test
-    public void simpleTest(){
+    public void simpleTest() {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
         driver = new ChromeDriver();
 
-        String  eTitle = "Meet Guru99",
+        String eTitle = "Meet Guru99",
                 aTitle = "";
 
         //start browser and go to adres
@@ -102,12 +132,12 @@ public class SimpleUITestChrome {
 
 
     @BeforeTest
-    public void loginInJiraTest(){
+    public void loginInJiraTest() {
 //        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
 //        driver = new ChromeDriver();
 
-        String  eTitle = "Log in - JIRA",
+        String eTitle = "Log in - JIRA",
                 aTitle = "";
 
         //start browser and go to adres
@@ -146,7 +176,7 @@ public class SimpleUITestChrome {
     }
 
     @Test
-    public void exitFromJiraTest(){
+    public void exitFromJiraTest() {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
 
@@ -162,6 +192,4 @@ public class SimpleUITestChrome {
     }
 
 
-
-
-}//class 
+}//class
