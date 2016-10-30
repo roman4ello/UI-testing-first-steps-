c5e461ffe52c57a0d59a93ce1a73cf74e91c4666
Boyar Roman <br777roman@gmail.com>
2016-10-30 21:45:14 +0200

    6 commit : 1.Разнесено по слоям 2. Детализация методов (разбивка на мелкие) удобство для дебага 3. Все пути вынесены в поля

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -7,96 +7,48 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by ramzes on 27.10.2016.
  */
 public class LoginPage {
     private WebDriver driver;
-    //    private WebElement webElement;
     private Date date = new Date();
     private DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+    private static final String loginURL = "http://soft.it-hillel.com.ua:8080/login.jsp";
+    private static final String fieldLoginUserName = "//input[@id='login-form-username']";
+    private static final String fieldLoginPassword = "//input[@id='login-form-password']";
+    private static final String fieldLoginSubmit = "//input[@id='login-form-submit']";
+    private static final String headersUserFullName = "//*[@id='header-details-user-fullname']";
+    private static final String buttonLogOut = "//*[@id='log_out']";
+    private static final String buttonCreateLink = "//*[@id='create_link']";
+    private static final String projectField = "//*[@id='project-field']";
+    private static final String fieldSummary = "//*[@id='summary']";
+    private static final String reporterField = "//*[@id='reporter-field']";
+    private static final String descriptionField = "//*[@id='description']";
+    private static final String buttonCreateIssueSubmit = "//*[@id='create-issue-submit']";
+    private static final String popUPWindow = "//*[@id='aui-flag-container']/div/div";
+    private static final String userProfileImage = "//*[@id=\"header-details-user-fullname\"]/span/span/img";
+    private static final String logOutWindow = "//*[@id='user-options']/a";
 
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
-//        System.setProperty("webdriver.gecko.driver", "geckodriver");
     }
 
-//
-//    public LoginPage(FirefoxDriver driverFirefox) {
-//        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
-//        this.driver = new FirefoxDriver();
-//    }
-
-    public String login(String loginName, String password) throws InterruptedException {
-        this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public void login(String loginName, String password) throws InterruptedException {
         this.driver.manage().window().maximize();
+        this.driver.get(loginURL);
+        findElementByXpath(fieldLoginUserName).sendKeys(loginName);
+        findElementByXpath(fieldLoginPassword).sendKeys(password);
+        findElementByXpath(fieldLoginSubmit).click();
 
-        //Search element
-        this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        this.driver.get("http://soft.it-hillel.com.ua:8080/login.jsp");
-        this.driver.findElement(By.xpath("//input[@id='login-form-username']")).sendKeys(loginName);
-        this.driver.findElement(By.xpath("//input[@id='login-form-password']")).sendKeys(password);
-        this.driver.findElement(By.xpath("//input[@id='login-form-submit']")).click();
-
-        while (!this.driver.getTitle().contains("System Dashboard - JIRA")) {
-            Thread.sleep(100);
-        }
-        System.out.println(this.driver.getTitle());
-
-        return this.driver.getTitle();
     }
 
-    public String getTitle() {
-        return this.driver.getTitle();
-    }
-
-
-    public long exit() {
-        this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        this.driver.findElement(By.xpath("//*[@id='header-details-user-fullname']")).click();
-        this.driver.findElement(By.xpath("//*[@id='log_out']")).click();
-        this.driver.close();
-        System.out.println("Удачно разлогинился и закрыл браузер");
-        return 0;
-    }
-
-    public void clickCreateNewIssue() {
-        //Search element
-        driver.findElement(By.xpath("//*[@id='create_link']")).click();
-        // sleep
-    }
-
-    public void enterProjectName() {
-        WebElement projectNameInput = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='project-field']")));
-        projectNameInput.click();
-        projectNameInput.sendKeys("QAAutomation2 (QAAUT)");
-        projectNameInput.submit();
-    }
-
-    public void enterSummary(String issueName) {
-        WebElement summary = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='summary']")));
-        summary.sendKeys(issueName);
-    }
-
-    public void enterReporter(String reporterName) {
-        WebElement reporterInputField = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='reporter-field']")));
-        reporterInputField.click();
-        reporterInputField.sendKeys(reporterName);
-    }
-
-    public void enterDescription(String description) {
-        driver.findElement(By.xpath("//*[@id='description']")).sendKeys(description);
-    }
-
-    public void clickSubmit() {
-        driver.findElement(By.xpath("//*[@id='create-issue-submit']")).click();
-        // Thread.sleep(2000);
+    public void logOut() {
+        waitForWebElementUntilElementToBeClickable(headersUserFullName).click();
+        waitForWebElementUntilPresenceOfElementLocated(buttonLogOut).click();
     }
 
     public void createIssue(String issueName, String reporterName) throws InterruptedException {
@@ -109,18 +61,82 @@ public class LoginPage {
         clickSubmit();
     }
 
+
+    //--------------------------------------------------------------------------------------------
+    //checks booleans methods (проверки)
     public boolean popupWindowIsPresent() {
-        boolean createIssue = false;
+        return webElementIsPresent(popUPWindow);
+    }
 
-        if (driver.findElement(By.xpath("//*[@id='aui-flag-container']/div/div")).isEnabled()) {
-            WebElement popupWindow = driver.findElement(By.xpath("//*[@id='aui-flag-container']/div/div"));
-//            System.out.println("Создали Issue  = " + fullIssueNamewithDate);
-            createIssue = true;
-        }
+    public boolean userProfileImageIsPresent() {
+        return webElementIsPresent(userProfileImage);
+    }
 
-//        System.out.println("You successfully have created new ISSUE by NAME: " + fullIssueNamewithDate);
+    public boolean isLogOutWindowPresent() {
+        boolean isLogOutwindowPresent = webElementIsPresent(logOutWindow);
+        this.driver.close();
+        System.out.println("Удачно разлогинился и закрыл браузер");
+        return isLogOutwindowPresent;
+    }
 
-        return createIssue;
+    //private methods
+    //for createIssue
+    private void clickCreateNewIssue() {
+        waitForWebElementUntilPresenceOfElementLocated(buttonCreateLink)
+                .click();
+    }
+
+    private void enterProjectName() {
+        WebElement projectNameInput =
+                waitForWebElementUntilPresenceOfElementLocated(projectField);
+        projectNameInput.click();
+        projectNameInput.sendKeys("QAAutomation2 (QAAUT)");
+        projectNameInput.submit();
+    }
+
+    private void enterSummary(String issueName) {
+        waitForWebElementUntilElementToBeClickable(fieldSummary).
+                sendKeys(issueName);
+    }
+
+    private void enterReporter(String reporterName) {
+        WebElement reporterInputField =
+                waitForWebElementUntilPresenceOfElementLocated(reporterField);
+        reporterInputField.click();
+        reporterInputField.sendKeys(reporterName);
+    }
+
+    private void enterDescription(String description) {
+        waitForWebElementUntilPresenceOfElementLocated(descriptionField).
+                sendKeys(description);
+    }
+
+    private void clickSubmit() {
+        findElementByXpath(buttonCreateIssueSubmit).click();
+    }
+
+
+    //short and comfortable form for find element
+    private WebElement findElementByXpath(String xPath) {
+        return this.driver.findElement(By.xpath(xPath));
+    }
+
+    //return true if webElement has appeared
+    private boolean webElementIsPresent(String xPath) {
+        return waitForWebElementUntilPresenceOfElementLocated(xPath).isEnabled();
+    }
+
+    //wait for webElement until that appears( wait during 10 sec)
+    private WebElement waitForWebElementUntilPresenceOfElementLocated(String xPath) {
+        return (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xPath)));
+
+    }
+
+    private WebElement waitForWebElementUntilElementToBeClickable(String xPath) {
+        return (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(xPath)));
+
     }
 
 }//class
